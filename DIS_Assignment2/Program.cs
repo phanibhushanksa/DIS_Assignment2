127cc65697c75742c5cff01c1e71c96fd9bf746b
Srinivasa Arudra Phani Bhushan Kolla <bhushankolla@usf.edu>
2020-02-15 18:28:41 -0500

    Add project files.

---
+++ DIS_Assignment2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DIS_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[] { 5, 6, 6, 9, 9, 12 };
+            foreach(int e in marks)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void targetRange(int[] marks, int target)
+        {
+           
+        }
+    }
+}
